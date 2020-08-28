a1bdc8cea15159f5b3d03f376b8f95dcbd2d82e0
hampussle <hlinderstam@gmail.com>
2020-08-28 14:26:14 +0200

    Session03Commit02

--- Session03/Session03Exercise02/Program.cs
+++ Session03/Session03Exercise02/Program.cs
@@ -8,14 +8,29 @@ namespace Session03Exercise02
         {
             Console.WriteLine("Ange ett antal siffror, separerat med kommatecken. ");
             var input = Console.ReadLine();
+            var inputArray = input.Split(",", StringSplitOptions.RemoveEmptyEntries);
+            double?[] numberArray = new double?[inputArray.Length];
 
-            var inputArray = input.Split(".", StringSplitOptions.RemoveEmptyEntries);
-
-            foreach (var number in inputArray)
+            //konvertera sträng till int och spara i array
+            for (var i = 0; i < inputArray.Length; i++)
             {
-                Console.WriteLine("Värdet är " + number);
-                Console.ReadLine();
+                try
+                {
+                    numberArray[i] = double.Parse(inputArray[i]);
+                }
+                catch(Exception)
+                {
+                    Console.WriteLine("Misslyckat");
+                }
             }
+
+            //skriv ut resultat
+            for (var i = 0; i < numberArray.Length; i++)
+            {
+                Console.WriteLine(numberArray[i]);
+            }
+            //Console.WriteLine("Största nummer: " + Math.Ceiling(numberArray));
+            //Console.WriteLine("Minsta nummer: " + Math.Floor(numberArray));
         }
     }
 }
